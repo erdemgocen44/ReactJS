009111ec4904ca455176f2c093496cbcf4293e95
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2021-10-06 19:53:02 +0200

    react baslangic

---
+++ 01-my-app/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import User from "./components/User"
+
+const App = () => {
+  return (
+    <div>
+      <div className="App">
+        <h4>App Component</h4>
+        <User/>
+        <User/>
+        <User/>
+        <User/>
+        <User/>
+        <User/>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ 01-my-app/src/components/hello/Hello.js
@@ -0,0 +1,5 @@
+const Hello=()=>{
+    return <div>Bu benim ilk componenim</div>
+}
+
+export default Hello;
