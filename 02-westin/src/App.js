98b7240354b21d3f48333c4f0223ac496b770e88
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2021-10-09 16:03:52 +0200

    props

--- 02-westin/src/App.js
+++ 02-westin/src/App.js
@@ -1,5 +1,28 @@
-function App() {
-  return <div className="App">Merhaba Dünya</div>;
-}
+import React from "react";
+import Preloader from "./component/Preloader";
+import Header from "./component/Header";
+import Banner from "./component/Banner";
+import About from "./component/About";
+import Services from "./component/Services";
+import Portfolio from "./component/Portfolio";
+import Testimonal from "./component/Testimonal";
+import Blog from "./component/Blog";
+import Contact from "./component/Contact";
+
+const App = () => {
+  return (
+    <div>
+      <Preloader />
+      <Header />
+      <Banner />
+      <About />
+      <Services />
+      <Portfolio />
+      <Testimonal />
+      <Blog />
+      <Contact />
+    </div>
+  );
+};
 
 export default App;
