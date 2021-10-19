a2b4cd0f9f1e937b3db10d872173be9a80973bc6
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2021-10-19 19:08:37 +0200

    mentoring review

---
+++ mentoring/src/App.js
@@ -0,0 +1,18 @@
+import ExternalJS from "./component/mnt1/ExternalJS";
+import Hello from "./component/mnt1/Hello";
+import Inline from "./component/mnt1/Inline";
+import Internal from "./component/mnt1/Internal";
+import Review from "./component/review/Review";
+function App() {
+  return (
+    <div className="App">
+      {/* <Hello />
+      <Inline />
+      <Internal />
+      <ExternalJS /> */}
+      <Review />
+    </div>
+  );
+}
+
+export default App;
