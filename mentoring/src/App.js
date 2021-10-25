b4e30f9571b04d4e3af605375f33933f4c2655bc
erdemgocen44 <78229631+erdemgocen44@users.noreply.github.com>
2021-10-25 19:35:13 +0200

    hava durumu

--- mentoring/src/App.js
+++ mentoring/src/App.js
@@ -2,15 +2,19 @@ import ExternalJS from "./component/mnt1/ExternalJS";
 import Hello from "./component/mnt1/Hello";
 import Inline from "./component/mnt1/Inline";
 import Internal from "./component/mnt1/Internal";
+import Clock2 from "./component/mnt3/Clock2";
 import Review from "./component/review/Review";
+import Slider from "./component/slider/Slider";
 function App() {
   return (
     <div className="App">
       {/* <Hello />
       <Inline />
       <Internal />
-      <ExternalJS /> */}
+      <ExternalJS /> 
       <Review />
+      <Slider />*/}
+      <Clock2 yaziRengi="yellow" zeminRengi="grey" />
     </div>
   );
 }
